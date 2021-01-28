6c972edebb1218cc6d38a7e65558843bed218491
Kiddo <pjfariakiddo@gmail.com>
2021-01-28 00:02:37 -0400

    Base de trabajo

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+
+@Module({
+  imports: [AuthModule, UsersModule, TypeOrmModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
